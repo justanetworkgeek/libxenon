36afb37c6de6582977ce72172ca2dbd0769b7e3a
Mitchell Waite <mitchellwaite@users.noreply.github.com>
2025-03-15 13:33:18 -0400

    Fix ECC calculation code for XeLL update to prevent console brick
    Reverts ECC calculation data change made by
    
    https://github.com/Free60Project/libxenon/commit/9aeeefd897f413ddbec4409b6f12cfcd1540d168

--- libxenon/drivers/xenon_nand/xenon_sfcx.c
+++ libxenon/drivers/xenon_nand/xenon_sfcx.c
@@ -202,7 +202,7 @@ void sfcx_calcecc_ex(unsigned int *data, unsigned char* edc) {
 	val = ~val;
 
 	// 26 bit ecc data
-	edc[0] = ((val << 6) | (data[0x20C] & 0x3F)) & 0xFF;
+	edc[0] = ((val << 6) | (data[0xC] & 0x3F)) & 0xFF;
 	edc[1] = (val >> 2) & 0xFF;
 	edc[2] = (val >> 10) & 0xFF;
 	edc[3] = (val >> 18) & 0xFF;
